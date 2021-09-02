b4c8c13749b68a8223c268911ea83bb87d8942a9
AledPeart <aledpeart@gmail.com>
2021-09-02 10:41:39 +0000

    Added function to update surplus data to the sheet

--- run.py
+++ run.py
@@ -73,7 +73,7 @@ def calculate_surplus_data(sales_row):
     - Negative surplus indicates extra made when stock was sold out.
     '''
 
-    print("Calculating surplus data.../n")
+    print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1] #slices and returns the last row from "stock"
 
@@ -84,6 +84,17 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def update_surplus_worksheet(data):
+    '''
+    update surplus worksheet, adds a new row with new surplus data
+    '''
+
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated succesfully.\n")
+
+
 def main():
     '''
     Run all program functions
@@ -93,6 +104,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
